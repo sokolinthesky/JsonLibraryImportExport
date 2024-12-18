b9fc81daff1c1c6e2e03d5fa6d68cfea93625512
Oleh S <soklakov.o.yu@gmail.com>
2024-12-18 17:53:45 +0100

    init commit

---
+++ JsonLibraryImportExport.cs
@@ -0,0 +1,114 @@
+﻿using JsonLibraryImportExport.View;
+using JsonLibraryImportExport.ViewModel;
+using Playnite.SDK;
+using Playnite.SDK.Events;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JsonLibraryImportExport
+{
+    public class JsonLibraryImportExport : GenericPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private JsonLibraryImportExportSettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("888ab97e-ea1b-40e5-a2da-ef917aee0603");
+
+        public JsonLibraryImportExport(IPlayniteAPI api) : base(api)
+        {
+            settings = new JsonLibraryImportExportSettingsViewModel(this);
+            Properties = new GenericPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override IEnumerable<MainMenuItem> GetMainMenuItems(GetMainMenuItemsArgs args)
+        {
+            return new List<MainMenuItem>
+            {
+                new MainMenuItem
+                {
+                    Description = "Open import export window",
+                    MenuSection = "@Json Library Import Export",
+                    Action = _ =>
+                    {
+                        OpenImportExportWindow();
+                    }
+                }
+            };
+        }
+
+        private void OpenImportExportWindow()
+        {
+            var window = PlayniteApi.Dialogs.CreateWindow(new WindowCreationOptions
+            {
+                ShowMinimizeButton = false,
+                ShowMaximizeButton = true
+            });
+            window.Height = 150;
+            window.Width = 450;
+            window.Title = "Json Library Import Export";
+            window.Content = new JsonLibraryImportExportView();
+            window.DataContext = new JsonLibraryImportExportViewModel(PlayniteApi, settings);
+            window.Owner = PlayniteApi.Dialogs.GetCurrentAppWindow();
+            window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            window.ShowDialog();
+        }
+
+        public override void OnGameInstalled(OnGameInstalledEventArgs args)
+        {
+            // Add code to be executed when game is finished installing.
+        }
+
+        public override void OnGameStarted(OnGameStartedEventArgs args)
+        {
+            // Add code to be executed when game is started running.
+        }
+
+        public override void OnGameStarting(OnGameStartingEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameStopped(OnGameStoppedEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameUninstalled(OnGameUninstalledEventArgs args)
+        {
+            // Add code to be executed when game is uninstalled.
+        }
+
+        public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
+        {
+            // Add code to be executed when Playnite is initialized.
+        }
+
+        public override void OnApplicationStopped(OnApplicationStoppedEventArgs args)
+        {
+            // Add code to be executed when Playnite is shutting down.
+        }
+
+        public override void OnLibraryUpdated(OnLibraryUpdatedEventArgs args)
+        {
+            // Add code to be executed when library is updated.
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new JsonLibraryImportExportSettingsView();
+        }
+    }
+}
---
+++ ViewModel/JsonLibraryImportExportViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Playnite.SDK;
+using Newtonsoft.Json;
+using Playnite.SDK.Models;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JsonLibraryImportExport.ViewModel
+{
+    class JsonLibraryImportExportViewModel
+    {
+        private readonly IPlayniteAPI playniteApi;
+
+        public JsonLibraryImportExportSettingsViewModel Settings { get; }
+
+        public JsonLibraryImportExportViewModel(IPlayniteAPI playniteApi, JsonLibraryImportExportSettingsViewModel settings)
+        {
+            this.playniteApi = playniteApi;
+            Settings = settings;
+        }
+
+        public RelayCommand ExportAllGamesCommand
+        {
+            get => new RelayCommand(() =>
+            {
+                ExportGamesToJson();
+            });
+        }
+
+        public void ExportGamesToJson()
+        {
+            var folderPath = playniteApi.Dialogs.SelectFolder();
+            if (string.IsNullOrEmpty(folderPath))
+            {
+                return;
+            }
+            IEnumerable<Game> games = playniteApi.Database.Games;
+            var jsonString = JsonConvert.SerializeObject(games, Formatting.Indented);
+            string fullPathGames = Path.Combine(folderPath, "games.json");
+            File.WriteAllText(fullPathGames, jsonString);
+        }
+
+        public RelayCommand ImportAllGamesCommand
+        {
+            get => new RelayCommand(() =>
+            {
+                ImportGamesFromJson();
+            });
+        }
+
+        public void ImportGamesFromJson()
+        {
+            var filePath = playniteApi.Dialogs.SelectFile("json|*.json");
+            var jsonString = File.ReadAllText(filePath);
+
+            IEnumerable<Game> games = JsonConvert.DeserializeObject<IEnumerable<Game>>(jsonString);
+
+            foreach (var game in games)
+            {
+                if (!playniteApi.Database.Games.Contains(game))
+                {
+                    playniteApi.Database.Games.Add(game);
+                }
+                else
+                {
+                    playniteApi.Database.Games.Update(game);
+                }
+            }
+        }
+    }
+}
